c16f6c24a8b55e8f7723c170c71058b73009a3b2
Sam Thabet <sallam.thabet@gmail.com>
2022-01-24 23:51:02 -0500

    added filter for single site inquiry corrected naming

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ const cryptoSites = [
     base: "https://www.coindesk.com",
   },
   {
-    name: "CNBC Finance",
+    name: "CNBCFinance",
     address: "https://www.cnbc.com/finance/",
     base: "https://www.cnbc.com",
   },
   {
-    name: "CNN Business",
+    name: "CNNBusiness",
     address: "https://www.cnn.com/business",
     base: "https://www.cnn.com",
   },
   {
-    name: "Today On Chain",
+    name: "TodayOnChain",
     address: "https://www.todayonchain.com/",
     base: "",
   },
@@ -75,8 +75,51 @@ app.get("/", (req, res) => {
 app.get("/news", (req, res) => {
   res.json(articles);
 });
-app.get('/news/:cryptoSiteId', async()=> {
+//Getting the information for a specific site
+app.get("/news/:cryptoSiteId", async (req, res) => {
+  //Getting the value of cryptoSiteId and setting it to siteId
+  const siteId = req.params.cryptoSiteId;
+
+  //This is to filter out the siteId address to the params that is requested
+  const siteAddress = cryptoSites.filter(
+    (cryptoSite) => cryptoSite.name == siteId
+  )[0].address;
+
+//This is to establish the articles base site in the case that it is required for the filtered site
+  const siteBase = cryptoSites.filter(
+    (cryptoSite) => cryptoSite.name == cryptoSiteId
+  )[0].base;
   
-})
+  //
+
+  axios.get(siteAddress).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const specificArticles = [];
+
+    $('a:contains("Bitcoin")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+
+      // Pushing the acquired article title, url and source into articles array
+      specificArticles.push({
+        title,
+        url: cryptoSite.base + url,
+        source: cryptoSite.name,
+      });
+    });
+    //Searching for word crypto in a-tag and pushing the title and webpage link into the array named articles
+    $('a:contains("Crypto")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+      // Pushing the acquired article title, url and source into articles array
+      specificArticles.push({
+        title,
+        url: cryptoSite.base + url,
+        source: cryptoSite.name,
+      });
+    });
+  });
+});
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
